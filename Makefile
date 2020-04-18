515affaf3d5f2ba8b38d36a7a9b6f4c43ffc4426
Shubh <flummoxedshubh@gmail.com>
2020-04-19 00:15:12 +0530

    Restart

--- Makefile
+++
@@ -1,8 +0,0 @@
-CC=g++
-CFLAGS=-Wall
-
-all:program
-
-program:main.cpp
-	$(CC) $(CFLAGS) main.cpp -o program
-
--- main.cpp
+++ main.cpp
@@ -1,20 +1,31 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_VAL 255
 
 int main()
 {
-	int nx = 200;
-	int ny = 100;
-	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-	for(int j = ny-1; j >=0; j--){
-		for(int i = 0; i < nx; i++){
-		float r = float(i) / float(nx);
-		float g = float(j) / float(ny);
-		float b = 0.2;
-		float ir = int(255.99*r);
-		float ig = int(255.99*g);
-		float ib = int(255.99*b);
-		std::cout << ir << " " << ig << " " << ib << "\n"; 
-		}
-	}
-	return 0;
+    const int image_width = 1920;
+    const int image_height = 1080;
+
+    FILE* file_ptr = fopen("img.ppm", "w");
+
+    fprintf(file_ptr, "P3\n%i %i\n%i\n", image_width, image_height, MAX_VAL);
+
+    for(int rows = image_height-1; rows >= 0; --rows){
+        for(int columns = 0; columns < image_width; ++columns){
+            auto r = double(columns) / image_width;
+            auto g = double(rows) / image_height;
+            auto b = 0.2;
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+            fprintf(file_ptr, "%i %i %i ", ir, ig, ib);
+        }
+        fprintf(file_ptr, "\n");
+    }
+
+    fclose(file_ptr);
+
+    return 0;
 }
