16c9720fccf18139360b8b9d65658c30f52a001f
Shubh <flummoxedshubh@gmail.com>
2020-06-10 20:27:44 +0530

    Final commit

--- camera.h
+++ camera.h
@@ -5,15 +5,41 @@
 
 class camera{
 public:
-    camera() {
-        lower_left_corner = point3(-2.0, -1.0, -1.0);
-        horizontal = vec3(4.0, 0.0, 0.0);
-        vertical = vec3(0.0, 2.0, 0.0); //Not Exactly in ratio (16:9)
-        origin = point3(0.0, 0.0, 0.0);
-    }
+    camera(
+        point3 lookfrom,
+        point3 lookat,
+        vec3   vup,
+        double vfov, // vertical field-of-view in degrees
+        double aspect_ratio,
+        double aperture,
+        double focus_dist) {
+            auto theta = degrees_to_radians(vfov);
+            auto h = tan(theta/2);
+            auto viewport_height = 2.0 * h;
+            auto viewport_width = aspect_ratio * viewport_height;
 
-    ray get_ray(double u, double v) const {
-        return ray(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            auto focal_length = 1.0;
+
+            w = unit_vector(lookfrom - lookat);
+            u = unit_vector(cross(vup, w));
+            v = cross(w, u);
+
+            origin = lookfrom;
+            horizontal = focus_dist * viewport_width * u;
+            vertical = focus_dist * viewport_height * v;
+            lower_left_corner = origin - horizontal/2 - vertical/2 - focus_dist*w;
+
+            lens_radius = aperture / 2;
+        }
+
+    ray get_ray(double s, double t) const {
+        vec3 rd = lens_radius * random_in_unit_disk();
+        vec3 offset = u * rd.x() + v * rd.y();
+
+        return ray(
+            origin + offset,
+            lower_left_corner + s*horizontal + t*vertical - origin - offset
+        );
     }
 
 private:
@@ -21,6 +47,8 @@ private:
     point3 lower_left_corner;
     vec3 horizontal;
     vec3 vertical;
+    vec3 u, v, w;
+    double lens_radius;
 };
 
 #endif //CAMERA_H
--- sphere.h
+++ sphere.h
@@ -6,13 +6,15 @@
 class sphere: public hittable{
     public:
     sphere() {}
-    sphere(point3 cen, double r): center(cen), radius(r) {}
+    sphere(point3 cen, double r, shared_ptr<material> m)
+        : center(cen), radius(r), mat_ptr(m) {};
 
     virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const;
 
     public:
     point3 center;
     double radius;
+    shared_ptr<material> mat_ptr;
 };
 
 bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const{
@@ -31,6 +33,7 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
             //Normalize (vector from center of circle to point of intersection) with it's magnitude ie Radius.
             vec3 outward_normal = (rec.p - center) / radius; 
             rec.set_face_normal(r, outward_normal);
+            rec.mat_ptr = mat_ptr;
             return true;
         }
         temp = (-half_b + root) / a;
@@ -40,6 +43,7 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
             //Normalize (vector from center of circle to point of intersection) with it's magnitude ie Radius.
             vec3 outward_normal = (rec.p - center) / radius; 
             rec.set_face_normal(r, outward_normal);
+            rec.mat_ptr = mat_ptr;
             return true;
         }
     }
